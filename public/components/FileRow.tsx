319089954d907deabaf0aa2224beb4eb49f83599
Ken Mueller <kenmueller0@gmail.com>
2020-07-16 16:01:10 -0700

    name input

--- public/components/FileRow.tsx
+++ public/components/FileRow.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction } from 'react'
+import { SetStateAction, useState, useCallback, ChangeEvent, FormEvent, useRef } from 'react'
 import cx from 'classnames'
 
 import { File } from 'hooks/useFiles'
@@ -11,12 +11,47 @@ export interface FileRowProps {
 }
 
 const FileRow = ({ file, setFiles }: FileRowProps) => {
+	const nameInput = useRef(null as HTMLInputElement | null)
+	
+	const [name, setName] = useState(file.name)
+	
+	const onNameChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+		setName(event.target.value)
+	}, [setName])
+	
+	const onNameSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		
+		setFiles(files =>
+			files.map(_file => ({
+				..._file,
+				name: _file.id === file.id ? name : _file.name
+			}))
+		)
+		
+		nameInput.current.blur()
+	}, [setFiles, file.id, name])
+	
 	return (
-		<tr className={styles.root}>
-			<td className={cx(styles.icon, {
-				[styles.image]: file.image
-			})}>
-				
+		<tr>
+			<td
+				className={cx(styles.icon, {
+					[styles.noImage]: !file.image
+				})}
+				style={
+					file.image
+						? { background: `url(${file.url})` }
+						: undefined
+				}
+			/>
+			<td className={styles.name}>
+				<form onSubmit={onNameSubmit}>
+					<input
+						ref={nameInput}
+						value={name}
+						onChange={onNameChange}
+					/>
+				</form>
 			</td>
 		</tr>
 	)
