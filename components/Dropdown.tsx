591803d67301cf042f5b55a466bc7b53d20e4ccd
Ken Mueller <kenmueller0@gmail.com>
2020-11-29 19:50:28 -0800

    hide dropdown

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState, useCallback } from 'react'
+import { ReactNode, useState, useCallback, useEffect, useRef } from 'react'
 import cx from 'classnames'
 
 import styles from 'styles/Dropdown.module.scss'
@@ -12,18 +12,41 @@ export interface DropdownProps {
 }
 
 const Dropdown = ({ rootClassName, triggerClassName, contentClassName, trigger, children }: DropdownProps) => {
+	const triggerRef = useRef<HTMLButtonElement | null>(null)
+	const contentRef = useRef<HTMLDivElement | null>(null)
+	
 	const [isShowing, setIsShowing] = useState(false)
 	
-	const show = useCallback(() => {
-		setIsShowing(true)
-	}, [setIsShowing])
+	const onClick = useCallback((event: MouseEvent) => {
+		const trigger = triggerRef.current
+		const content = contentRef.current
+		const target = event.target as Node
+		
+		if (!(trigger && content))
+			return
+		
+		if (trigger === target || trigger.contains(target))
+			return setIsShowing(isShowing => !isShowing)
+		
+		if (content === target || content.contains(target))
+			return
+		
+		setIsShowing(false)
+	}, [triggerRef, contentRef, setIsShowing])
+	
+	useEffect(() => {
+		const { body } = document
+		
+		body.addEventListener('click', onClick)
+		return () => body.removeEventListener('click', onClick)
+	}, [onClick])
 	
 	return (
 		<div className={cx(styles.root, rootClassName)}>
-			<button className={cx(styles.trigger, triggerClassName)} onClick={show}>
+			<button className={cx(styles.trigger, triggerClassName)} ref={triggerRef}>
 				{trigger}
 			</button>
-			<div className={cx(styles.content, contentClassName)} aria-hidden={!isShowing}>
+			<div className={cx(styles.content, contentClassName)} ref={contentRef} aria-hidden={!isShowing}>
 				{children}
 			</div>
 		</div>
