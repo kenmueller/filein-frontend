be9ebf7edd70d69cc1f25419dc187d540c660a61
Ken Mueller <kenmueller0@gmail.com>
2020-12-02 13:36:25 -0800

    different secure link

--- lib/getFileUrl.ts
+++ lib/getFileUrl.ts
@@ -1,6 +1,6 @@
 import FileMeta from 'models/FileMeta'
 
 const getFileUrl = (file: FileMeta, secure: boolean = false) =>
-	`http${secure ? 's' : ''}://u.filein.io/${file.id}`
+	`${secure ? 'https://storage.googleapis.com/' : 'http://'}u.filein.io/${file.id}`
 
 export default getFileUrl
