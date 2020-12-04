d31299c5ece1a0a79e72cfd7e5af6fb91f922323
Ken Mueller <kenmueller0@gmail.com>
2020-12-03 19:05:36 -0800

    env

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class CustomDocument extends Document {
 	render = () => (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<script
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+					data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_AD_CLIENT}
+					async
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
