3633b363f08f0a28625217ca03e602e4f4d687cb
Ken Mueller <kenmueller0@gmail.com>
2020-12-06 17:37:36 -0800

    removed google ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class CustomDocument extends Document {
 	render = () => (
 		<Html lang="en">
-			<Head>
-				<script
-					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-					data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_AD_CLIENT}
-					async
-				/>
-			</Head>
+			<Head />
 			<body>
 				<Main />
 				<NextScript />
