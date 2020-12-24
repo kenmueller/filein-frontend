8eb834d35be3dc3f036dae7fe750a9e6f751cbdc
Ken Mueller <kenmueller0@gmail.com>
2020-12-23 20:37:03 -0800

    renamed env var

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = ({ className }: FooterProps) => (
 			<p className={styles.copyright}>
 				Copyright &copy; 2020 <b>filein</b>. All rights reserved.
 			</p>
-			<a className={styles.slack} href={process.env.NEXT_PUBLIC_SLACK_URL} rel="noopener noreferrer" target="_blank">
+			<a className={styles.slack} href={process.env.NEXT_PUBLIC_SLACK_INSTALL_URL} rel="noopener noreferrer" target="_blank">
 				<img
 					src="https://platform.slack-edge.com/img/add_to_slack.png"
 					srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
