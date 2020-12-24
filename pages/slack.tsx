2624a1f93084fa0c6c4ede4043bdeded8d110132
Ken Mueller <kenmueller0@gmail.com>
2020-12-24 02:20:30 -0800

    slack page base

---
+++ pages/slack.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from 'next'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSlack } from '@fortawesome/free-brands-svg-icons'
+
+import Head from 'components/Head'
+import Gradient from 'components/Gradient'
+import Footer from 'components/Footer'
+
+import styles from 'styles/Slack.module.scss'
+
+const Slack: NextPage = () => (
+	<div className={styles.root}>
+		<Head
+			url="https://filein.io/slack"
+			title="Slack - filein"
+			description="Add filein to your Slack workspace"
+			storagePreconnect={false}
+		/>
+		<Gradient className={styles.header}>
+			<article className={styles.hero}>
+				<FontAwesomeIcon className={styles.titleIcon} icon={faSlack} />
+				<h1 className={styles.title}>
+					Slack + filein
+				</h1>
+				<p className={styles.subtitle}>
+					Upload files without leaving your workspace
+				</p>
+			</article>
+		</Gradient>
+		<Footer className={styles.footer} />
+	</div>
+)
+
+export default Slack
