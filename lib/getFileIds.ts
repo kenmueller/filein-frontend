f8cf657ddb9ca40b6e287e166296b61ac8bc8f62
kenmueller <kenmueller0@gmail.com>
2021-03-16 20:24:54 -0700

    limit files

--- lib/getFileIds.ts
+++ lib/getFileIds.ts
@@ -2,11 +2,18 @@ import firebase from './firebase'
 
 import 'firebase/firestore'
 
+const LIMIT = 1000
+
 const firestore = firebase.firestore()
 
-const getFileIds = async () =>
-	(await firestore.collection('files').get())
-		.docs
-		.map(({ id }) => id)
+const getFileIds = async () => {
+	const { docs } = await firestore
+		.collection('files')
+		.orderBy('uploaded', 'desc')
+		.limit(LIMIT)
+		.get()
+
+	return docs.map(({ id }) => id)
+}
 
 export default getFileIds
